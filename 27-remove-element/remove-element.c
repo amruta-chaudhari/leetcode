801831d942170a97f55fa1703e0c6ed2e41ba5b4
Amruta Chaudhari <143449315+amruta-chaudhari@users.noreply.github.com>
2023-10-29 21:00:50 -0400

    Time: 4 ms (48.93%) | Memory: 6.8 MB (21.84%) - LeetSync

---
+++ 27-remove-element/remove-element.c
@@ -0,0 +1,13 @@
+int removeElement(int* nums, int numsSize, int val){
+
+    int i = 0, j = 0;
+    while (i < numsSize){
+        if(nums[i] != val){
+            nums[j] = nums[i];
+            j++;
+        }
+        i++;
+    }
+
+    return j;
+}
